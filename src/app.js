1bb807139c2bea8b4ad35db5c57a7afc1080d4c1
Fisayo <aikomofisayo@gmail.com>
2019-11-17 14:12:21 +0100

    Create get routes and handle files

--- src/app.js
+++ src/app.js
@@ -11,8 +11,30 @@ app.set('view engine', 'ejs'); // set view engine to ejs
 // the public directory
 app.use(express.static(path.join(__dirname, 'public'))); 
 
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'})
+    res.render('index', { title: 'Account Summary', accounts })
+})
+
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings })
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking })
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit })
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] })
 })
 
 app.listen(process.env.PORT || 3000, () => {
