66d12ec7e675abf173e73983b87e6fc369f36eba
Fisayo <aikomofisayo@gmail.com>
2019-11-17 20:36:43 +0100

    Handle form data

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ app.set('view engine', 'ejs'); // set view engine to ejs
 
 // configure static directory, all our css/js for the client side is found in
 // the public directory
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 const accounts = JSON.parse(accountData);
@@ -35,7 +36,20 @@ app.get('/credit', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] })
-})
+});
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+   
+    accounts[req.body.from.value].balance -= req.body.amount.value;
+    accounts[req.body.to.value].balance += parseInt(req.body.amount.value);
+    let accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync('json/accounts.json', accountsJSON);
+    res.render('transfer', { message: 'Transfer Completed' });
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('PS Project Running on port 3000!');
